01d1f21d21d730db99c15998211c2d431ea4ca3c
BabiPunSociu <Nguyendung281002@gmail.com>
2023-06-06 23:52:17 +0700

    DungNguyen

---
+++ Bai18_ColorDialog/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace Bai18_ColorDialog
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnChonMau = new System.Windows.Forms.Button();
+            this.grbBackColor = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnChonMau);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.ForeColor = System.Drawing.Color.Red;
+            this.groupBox1.Location = new System.Drawing.Point(0, 389);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(800, 61);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // btnChonMau
+            // 
+            this.btnChonMau.Location = new System.Drawing.Point(256, 20);
+            this.btnChonMau.Name = "btnChonMau";
+            this.btnChonMau.Size = new System.Drawing.Size(270, 33);
+            this.btnChonMau.TabIndex = 0;
+            this.btnChonMau.Text = "&Chọn màu";
+            this.btnChonMau.UseVisualStyleBackColor = true;
+            this.btnChonMau.Click += new System.EventHandler(this.btnChonMau_Click);
+            // 
+            // grbBackColor
+            // 
+            this.grbBackColor.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.grbBackColor.Location = new System.Drawing.Point(0, 0);
+            this.grbBackColor.Name = "grbBackColor";
+            this.grbBackColor.Size = new System.Drawing.Size(800, 389);
+            this.grbBackColor.TabIndex = 1;
+            this.grbBackColor.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.grbBackColor);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Bài 18: Color Dialog";
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnChonMau;
+        private System.Windows.Forms.GroupBox grbBackColor;
+    }
+}
+
---
+++ Bai18_ColorDialog/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bai18_ColorDialog
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnChonMau_Click(object sender, EventArgs e)
+        {
+            ColorDialog colorDialog = new ColorDialog();    // Tạo hộp thoại hộp màu (có thể dùng kéo thả)
+            colorDialog.FullOpen = true;    // Hiển thị toàn bộ hộp thoại Color
+            // Hiển thị phần Define Custom Colors
+            colorDialog.SolidColorOnly = false;
+
+            if (colorDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                grbBackColor.BackColor = colorDialog.Color /* Get giá trị màu của colorDialog */;
+
+        }
+    }
+}
